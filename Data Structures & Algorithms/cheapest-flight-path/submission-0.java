09cb8c983b7a91a0b232e04bc7daa6f2916b4ad7
Akki19hzb <getakash.verma19@gmail.com>
2026-04-11 16:49:13 +0530

    Add: cheapest-flight-path - submission-0

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-0.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> adj = new ArrayList<>();
+        int INF = Integer.MAX_VALUE;
+        
+        for(int i=0; i<n; i++)  
+            adj.add(new ArrayList<>());
+
+        for(int[] f : flights){
+            adj.get(f[0]).add(new int[]{f[1], f[2]});
+        }
+
+        int[] price = new int[n];
+        Arrays.fill(price, INF);
+        price[src] =0;
+
+        Queue<int[]> pq =
+            new ArrayDeque<>(); //stops, price, node
+
+        pq.offer(new int[]{0, 0, src});
+
+        while(!pq.isEmpty()){
+            int[] cell = pq.poll();
+
+            int stops = cell[0];
+            int cost = cell[1];
+            int u = cell[2];
+
+            if(stops > k)
+                continue;
+                for(int[] node : adj.get(u)){
+                    int v = node[0];
+                    int costV = node[1];
+                    if(cost + costV < price[v]){
+                        price[v] = cost + costV;
+                        pq.offer(new int[]{stops+1, price[v], v});
+                    }
+                }
+            
+        }
+
+        return price[dst] != INF ? price[dst] : -1;    
+    }
+}
